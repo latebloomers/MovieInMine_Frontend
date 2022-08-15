41747c9fb553503851372ea2a60005217c112c29
hyeokho <honorable110@gmail.com>
2022-08-15 20:21:08 +0900

    init: init project

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -7,6 +7,9 @@ import HomePage from "../pages/HomePage";
 import AboutPage from "../pages/AboutPage";
 import MoviePage from "../pages/MoviePage";
 import CnemaCupPage from "../pages/CnemaCupPage";
+import Signin from "../pages/Signin";
+import Signup from "../pages/Signup";
+
 
 const Router = () => {
   // const HomePage: React.FC = () => {
@@ -18,8 +21,9 @@ const Router = () => {
         <Route path="/about" element={<AboutPage />} />
         <Route path="/movie" element={<MoviePage />} />
         <Route path="/cnemacup" element={<CnemaCupPage />} />
+        <Route path="/users/signin" element={<Signin />} />
+        <Route path="/users/signup" element={<Signup />} />
       </Routes>
-
   );
 };
 
